8ad82e53213fd9be315c3bdf908f7287680aff35
Kurt Rose <kurt@kurtrose.com>
2022-11-06 19:40:46 -0800

    adding meta data

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='common_cents',
+    version='0.1',
+    license='MIT',
+    author="Kurt Rose",
+    author_email='kurt@kurtrose.com',
+    packages=find_packages('common_cents'),
+    package_dir={'': 'common_cents'},
+    url='https://github.com/kurtbrose/common_cents',
+    keywords='',
+    install_requires=[],
+)
